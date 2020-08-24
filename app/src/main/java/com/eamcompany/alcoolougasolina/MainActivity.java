2ca98078a9c47df35ae32b9f6da3a9c277654d26
Emanuel A Macedo <contato.emanuel97@gmail.com>
2020-08-24 15:08:49 -0300

    first commit

---
+++ app/src/main/java/com/eamcompany/alcoolougasolina/MainActivity.java
@@ -0,0 +1,57 @@
+package com.eamcompany.alcoolougasolina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editPrecoAlcool, editPrecoGasolina;
+    private TextView textResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editPrecoAlcool = findViewById(R.id.editPrecoAlcool);
+        editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
+        textResultado = findViewById(R.id.textResultado);
+    }
+
+    public void calcularPreco(View view) {
+        String precoAlcool = editPrecoAlcool.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+        Boolean camposValidados = validarCampos(precoAlcool, precoGasolina);
+
+        if(camposValidados){
+            Double valorAlcool=Double.parseDouble(precoAlcool);
+            Double valorGasolina=Double.parseDouble(precoGasolina);
+
+            if(valorAlcool/valorGasolina>=0.7){
+                textResultado.setText("É melhor abastacer na Gasolina");
+            }
+            else{
+                textResultado.setText("É melhor abastacer no Álcool");
+            }
+        }else{
+            textResultado.setText("Preencha os preços primeiro!");
+        }
+    }
+
+    public Boolean validarCampos(String pAlcool, String pGasolina) {
+        Boolean camposValidados = true;
+
+        if (pAlcool == null || pAlcool.equals("")) {
+            camposValidados = false;
+        }else if(pGasolina == null || pGasolina.equals("")){
+            camposValidados = false;
+        }
+
+        return camposValidados;
+    }
+}
